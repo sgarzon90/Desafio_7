84cd448dbb2af258e2389d8ad9ae9b7f91d88c33
Santiago Garzon <santigg90@gmail.com>
2023-10-09 14:48:59 -0500

    Se agrega modificación del desafio-7 - Santiago Garzón Gómez

--- js/main.js
+++ js/main.js
@@ -1,102 +1,96 @@
+// Entrega 07
+
 let parrafo = document.querySelector("title");
 console.log(parrafo.innerHTML);
 
-let parrafo1 = document.querySelectorAll("dd");
-
-function sumar() {
+function comparar() {
     let participante = ""
     let contador = 0;
     for (let j = contador; j <= 3; j++) {
 
-        if (parrafo1[j].innerHTML != "" && j <= 1) {
-            participante += parrafo1[j].innerHTML + " ";
-        } else if (parrafo1[j].innerHTML != "" && j <= 3) {
-            participante += parrafo1[j].innerHTML.toUpperCase() + " ";
+        if (datos[j].innerHTML != "" && j <= 1) {
+            participante += datos[j].innerHTML + " ";
+        } else if (datos[j].innerHTML != "" && j <= 3) {
+            participante += datos[j].innerHTML.toUpperCase() + " ";
         }
     }
     console.log("Integrante 1: " + participante);
 
-
     let participante1 = ""
     let contador1 = 4;
     for (let k = contador1; k <= 7; k++) {
-        if (parrafo1[k].innerHTML != "" && k <= 5) {
-            participante1 += parrafo1[k].innerHTML + " ";
-        } else if (parrafo1[k].innerHTML != "" && k <= 7) {
-            participante1 += parrafo1[k].innerHTML.toUpperCase() + " ";
+        if (datos[k].innerHTML != "" && k <= 5) {
+            participante1 += datos[k].innerHTML + " ";
+        } else if (datos[k].innerHTML != "" && k <= 7) {
+            participante1 += datos[k].innerHTML.toUpperCase() + " ";
         }
     }
     console.log("Integrante 2: " + participante1);
+}
 
-    if (parrafo1[0].innerHTML == parrafo1[4].innerHTML) {
-        let color = prompt("Los nombres son iguales, por favor ingrese un color:");
-        let elementoConClase = document.querySelector("#primernombre");
-        let elementoConClase1 = document.querySelector("#segundonombre");
-        console.log("Los nombres son iguales");
-        elementoConClase.style.color = color;
-        elementoConClase1.style.color = color;
-    } else if (parrafo1[0].innerHTML == parrafo1[5].innerHTML) {
-        let color = prompt("Los nombres son iguales, por favor ingrese un color:");
-        let elementoConClase1 = document.querySelector("#primernombre");
-        let elementoConClase = document.querySelector("#segundonombre1");
-        console.log("Los nombres son iguales");
-        elementoConClase.style.color = color;
-        elementoConClase1.style.color = color;
-    } else if (parrafo1[1].innerHTML == parrafo1[4].innerHTML) {
-        let color = prompt("Los nombres son iguales, por favor ingrese un color:");
-        let elementoConClase1 = document.querySelector("#primernombre1");
-        let elementoConClase = document.querySelector("#segundonombre");
-        console.log("Los nombres son iguales");
-        elementoConClase.style.color = color;
-        elementoConClase1.style.color = color;
-    } else if (parrafo1[1].innerHTML == parrafo1[5].innerHTML) {
-        let color = prompt("Los nombres son iguales, por favor ingrese un color:");
-        let elementoConClase1 = document.querySelector("#primernombre1");
-        let elementoConClase = document.querySelector("#segundonombre1");
-        console.log("Los nombres son iguales");
-        elementoConClase.style.color = color;
-        elementoConClase1.style.color = color;
-    } else {
-        console.log("No hay coincidencias en los nombres");
+function buscarCoincidencias(datosDelIntegrante1, datosDelIntegrante2) {
+    for (let i = 0; i < datosDelIntegrante1.length; i++) {
+
+        if (i === 0 || i === 1 || i === 2 || i === 3) {
+            for (let j = 0; j < datosDelIntegrante2.length; j++) {
+
+                if (j === 0 || j === 1) {
+                    if (datosDelIntegrante1[i].innerHTML === datosDelIntegrante2[j].innerHTML) {
+                        let color = prompt("Los nombres son iguales, por favor ingrese un color para detectarlos:");
+                        datosDelIntegrante1[i].style.color = color;
+                        datosDelIntegrante2[j].style.color = color;
+                        console.log("Hubo coincidencias en los nombres, se diferenciarán por el color: " + color);
+                    }
+                }
+
+                if (j === 2 || j === 3) {
+                    if (datosDelIntegrante1[i].innerHTML === datosDelIntegrante2[j].innerHTML) {
+                        let color = prompt("Los apellidos son iguales, por favor ingrese un color para detectarlos:");
+                        datosDelIntegrante1[i].style.color = color;
+                        datosDelIntegrante2[j].style.color = color;
+                        console.log("Hubo coincidencias en los apellidos, se diferenciarán por el color: " + color);
+                    }
+                }
+            }
+        }
     }
 
-    if (window.confirm("¿Desea comparar los apellidos")) {
-        if (parrafo1[2].innerHTML == parrafo1[6].innerHTML) {
-            let colorapellido = prompt("Los apellidos son iguales, por favor ingrese un color:");
-            let apellido = document.querySelector("#primerapellido");
-            let apellido1 = document.querySelector("#segundoapellido");
-            console.log("Los apellidos son iguales");
-            apellido.style.color = colorapellido;
-            apellido1.style.color = colorapellido;
-        } else if (parrafo1[2].innerHTML == parrafo1[7].innerHTML) {
-            let colorapellido = prompt("Los apellidos son iguales, por favor ingrese un color:");
-            let apellido1 = document.querySelector("#primerapellido");
-            let apellido = document.querySelector("#segundoapellido1");
-            console.log("Los apellidos son iguales");
-            apellido.style.color = colorapellido;
-            apellido1.style.color = colorapellido;
-        } else if (parrafo1[3].innerHTML == parrafo1[6].innerHTML) {
-            let colorapellido = prompt("Los apellidos son iguales, por favor ingrese un color:");
-            let apellido1 = document.querySelector("#primerapellido1");
-            let apellido = document.querySelector("#segundoapellido");
-            console.log("Los apellidos son iguales");
-            apellido.style.color = colorapellido;
-            apellido1.style.color = colorapellido;
-        } else if (parrafo1[3].innerHTML == parrafo1[7].innerHTML) {
-            let colorapellido = prompt("Los apellidos son iguales, por favor ingrese un color:");
-            let apellido1 = document.querySelector("#primerapellido1");
-            let apellido = document.querySelector("#segundoapellido1");
-            console.log("Los apellidos son igualess");
-            apellido.style.color = colorapellido;
-            apellido1.style.color = colorapellido;
-        } else {
-            console.log("No hay coincidencias en los apellidos");
-        }
+}
+
+const datos = document.querySelectorAll("dd");
+const datosDelIntegrante1 = [datos[0], datos[1], datos[2], datos[3]];
+const datosDelIntegrante2 = [datos[4], datos[5], datos[6], datos[7]];
+
+function ejecutar() {
+    comparar();
+    buscarCoincidencias(datosDelIntegrante1, datosDelIntegrante2);
+}
+
+// Bonus
+
+function validarFormulario() {
+    let nombre = document.querySelector("#nombre").value;
+    let nombreuno = document.querySelector("#nombreuno").value;
+    const apellido = document.querySelector("#apellido").value;
+    const apellidouno = document.querySelector("#apellidouno").value;
+    const nombredos = document.querySelector("#nombredos").value;
+    const nombretres = document.querySelector("#nombretres").value;
+    const apellidodos = document.querySelector("#apellidodos").value;
+    const apellidotres = document.querySelector("#apellidotres").value;
+    console.log(nombre, nombreuno, apellido.toUpperCase(), apellidouno.toUpperCase());
+    console.log(nombredos, nombretres, apellidodos.toUpperCase(), apellidotres.toUpperCase());
+
+    if (nombre == nombredos || nombre == nombretres || nombreuno == nombredos || nombreuno == nombretres) {
+        console.log("Los nombres son iguales");
+        window.alert("Los nombres son iguales");
+    }
+
+    if (apellido == apellidotres || apellido == apellidodos || apellidouno == apellidodos || apellidouno == apellidotres) {
+        console.log("Los apellidos son iguales");
+        window.alert("Los apellidos son iguales");
     }
 }
 
-sumar();
-
-
-
-
+function compararbonus() {
+    validarFormulario();
+}
